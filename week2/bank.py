0a5e044bbec1dbe0945c14e39a92717ccdb6ec75
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-02-06 14:41:32 +0000

    Update bank.py

--- week2/bank.py
+++ week2/bank.py
@@ -1,6 +1,18 @@
-def main(): 
-    print('welcome to the balance app')#print statement to introduce the problem 
-    balance1 = input("first balance:  ")#take in user input for the first balance 
-    balance2 = input("second balence: ")#take in user input for the second balance 
-    sum = float(balance1) + float(balance2)# Adding two numbers
-    print("The sum of {0} and {1} is {2}" .format(balance1, balance2, sum))
+#print statement to introduce problem
+print("welcome to the balance calculator")
+
+#asking the user to input there name
+var = input('what is your name')
+# take inputs
+num1 = float(input('Enter first amount in cents: '))
+num2 = float(input('Enter second amount in cents: '))
+
+# adding numbers
+sum = num1 + num2
+
+
+# calculate sum of numbers
+('total amount of {0} and {1} {2}' .format(num1, num2,sum))
+
+# print sum plus user input 
+print(f"{var} your balance is \u20ac", sum)
