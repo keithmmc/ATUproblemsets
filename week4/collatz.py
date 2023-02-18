9600642589278b121151d707be0ca237c0dae09a
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-02-18 20:30:45 +0000

    Update collatz.py

--- week4/collatz.py
+++ week4/collatz.py
@@ -1,6 +1,31 @@
-def collatz(n):
-    while n != 1:
-        print(n, end = " ")
-        if n & 1:
-            n = 3 * n +1 
+def collatz(number):
+    ##check if the number is even or odd and performs calculations.
+    
+    if number % 2  == 0: # even
+        print(number // 2)
+        return number //2
+    elif number % 2 != 0: # odd 
+        result = 3*number+1
+        print(result)
+        return result
+
+try:
+    n = input('Enter number: ') # takes user input
+    while n !=1: # performs while loop until 'n' becomes 1
+        n = collatz(int(n)) # passes 'n' to collatz() function until it arrives at '1'
+except ValueError:
+    print('Value Error. Please enter integer.')
+
+
+
+
+        
+  
+
+
+
+
+
+
+
             
