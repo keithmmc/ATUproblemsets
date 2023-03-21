21ed344cf8258f11bbca23ebf918864a2fba139e
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-03-21 22:30:47 +0000

    Update plot.py
    updating latest attempt at problem for week one

--- week8/plot.py
+++ week8/plot.py
@@ -1,19 +1,42 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import statistics
-import numpy as np 
 
-d = [1, 1000]
+# Define the function h(x) = x^3
+def h(x):
+    return x ** 3
 
-plt.hist(d, bins= 11, color ='c')
-m = statistics.mean(d)
-sd = statistics.stdev(d)
+# Generate a range of x-values from 0 to 10
+x = np.linspace(0, 10)
 
-plt.axvline(m, color='b', linestyle = 'dashed')
-plt.axvline(m + sd, color='k', linestyle = 'dashed')
-plt.axvline(m - sd, color='k', linestyle = 'dashed')
-plt.xlabel("value")
-plt.ylabel("Frequency")
-plt.title("histogram")
+# Evaluate h(x) for each value of x
+y = h(x)
+
+# Create a plot of h(x) vs. x
+plt.plot(x, y)
+
+# Add labels to the x- and y-axes
+plt.xlabel('x')
+plt.ylabel('h(x)')
+
+# Set the title of the plot
+plt.title('Plot of h(x) = x^3 in the range [0, 10]')
+
+# Show the plot
+plt.show()
+
+
+# Generate 1000 random values from a normal distribution with mean 5 and standard deviation 2
+data = np.random.normal(5, 2, 1000)
+
+# Create a histogram
+plt.hist(data)
+
+# Add labels and a title
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.title('Histogram')
+
+# Show both of the plots
 plt.show()
 
 
