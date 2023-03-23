eb546b927ac3b21645752aff2cb090f69a8ac654
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-03-23 21:40:19 +0000

    Update python accounts.py

--- week3/python accounts.py
+++ week3/python accounts.py
@@ -1,8 +1,14 @@
+
+#asking the user to input a string to calulate a 10 degit account number 
 accountnumber = str(input("enter your 10 degit account number: "))
 
+#get the final four degits of the account number 
 final = accountnumber[-4:]
-
+##using the following to test the above line of code has worked
+##print(f"{final}")
+##using a print a statement to display the 6 x's and the final four digets of the account number 
 print(f"xxxxxx{final}")
+
 
 
 
