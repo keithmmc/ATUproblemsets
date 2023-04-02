ed380f854f14fba595c2f7adfd95fa004c516122
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-04-02 14:09:16 +0100

    Update sqaure.py

--- week6/sqaure.py
+++ week6/sqaure.py
@@ -1,4 +1,3 @@
-
 # create a fucntion 
 
 def sqrt(userinput):
@@ -15,11 +14,16 @@ def sqrt(userinput):
 userinput = input("Please enter a positive number:")
 print("The square root of", userinput, "is approx." , sqrt(userinput))
 
+    
+    
+
+
 
 # References:
 # https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
 # https://stackoverflow.com/questions/55232484/newtons-method-for-approximating-square-roots
 #  https://www.w3schools.com/python/python_functions.asp 
+# https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
 
 
 
