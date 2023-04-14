03d9b8bd8216eef1450b55692b59a937c28a3f58
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-04-14 23:56:15 +0100

    Update plot.py
    updated plot

--- week8/plot.py
+++ week8/plot.py
@@ -5,37 +5,25 @@ import matplotlib.pyplot as plt
 def h(x):
     return x ** 3
 
-# Generate a range of x-values from 0 to 10
-x = np.linspace(0, 10)
-
-# Evaluate h(x) for each value of x
+#Create an array of x values in the range 0, 10 and get y values using h(x) = x^3
+x = np.array(range(0, 10))
+print(x)
 y = h(x)
 
 # Create a plot of h(x) vs. x
-plt.plot(x, y)
-
-# Add labels to the x- and y-axes
-plt.xlabel('x')
-plt.ylabel('h(x)')
-
-# Set the title of the plot
-plt.title('Plot of h(x) = x^3 in the range [0, 10]')
-
-# Show the plot
-plt.show()
-
+plt.plot(x, y, label='Histogram', color="red")
 
 # Generate 1000 random values from a normal distribution with mean 5 and standard deviation 2
 data = np.random.normal(5, 2, 1000)
 
 # Create a histogram
-plt.hist(data)
+plt.hist(data, bins = 10, label = 'Normal Distribution', color = "blue")
 
 # Add labels and a title
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.title('Histogram')
-
+plt.xlabel('Value',fontsize="18",font = "Brush Script MT")
+plt.ylabel('Frequency',fontsize="18",font = "Brush Script MT")
+plt.title('Week 08 Task histogram',font = "Brush Script MT",fontsize="20",color="green")
+plt.legend()
 # Show both of the plots
 plt.show()
 
