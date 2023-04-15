cde1070767f93c40b72c561f45b5f6c6edf7ed70
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-04-15 11:03:24 +0100

    Update es.py

--- week 7/es.py
+++ week 7/es.py
@@ -1,22 +1,25 @@
 import sys 
-#asking the user to input the file for searching
-filename = input("Enter file name: ")
-#asking the user to enter the letter to be searched
-l=input("Enter letter to be searched:")
-k = 0
- #opens the text file
-with open(filename, 'r') as f:
-    for line in f:
-        words = line.split()
-# splits line to get list of words
-        for i in words:
-            for letter in i:
-                if(letter==l):
-                    k=k+1
-#print the occurrences of the letter 
-print("Occurrences of the letter:")
-print(k)
 
-#https://youtu.be/QJBVjBq4c7E
+filename = sys.argv[1]
+
+try:
+    with open(filename,'r') as f:
+       text = f.read()
+       count = 0
+       for i in text:
+         if (i == "e"or i =="E"):
+            count +=1
+       print(count)
+
+except FileNotFoundError:
+   print("filename (", filename,") does not exist. Please re-run and enter a valid filename", sep = '')
+
+
+
+
+# https://www.youtube.com/watch?v=decAHMKIo_A 
+#https://www.youtube.com/watch?v=QJBVjBq4c7E
+# https://www.delftstack.com/howto/python/occurrences-of-a-character-in-a-string-in-python/
+# https://www.cs2study.com/wp-content/uploads/2021/03/Text-File-exam-based-questions.pdf
 #https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
 #https://www.sanfoundry.com/python-program-read-file-counts-number/
