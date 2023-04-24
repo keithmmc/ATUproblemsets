115af6040b2de29e2e2740e2ebeb60f16bd206f8
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-04-24 11:05:48 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,6 +1,6 @@
 while True:
     try:
-        #using an if statement to get a postive number
+        #using an if statement to get a positive number if this is not entered it will ask the user to input a positive one
         num = int(input("Please enter a positive integer:"))
         if num <= 0:
             print("Invalid input. Please enter a positive integer.")
@@ -12,7 +12,7 @@ while True:
 while num !=1:
     #using end =' ' to print number on same line in terminal 
     print(num, end=' ')
-    # if it's an even number if no remainder
+    # if it's an even number
     if num % 2 == 0:
         num //= 2
     else:
